830d6b6be80b1336a2f6f5b6401ed65eb528e1f2
Owoyine Joseph <61910875+OwoyineJosephHastings@users.noreply.github.com>
2021-02-23 18:46:54 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <Route path="/" component={Nav} />
+        <Route path="/" component={Notes} />
         <PrivateRoute exact path="/notes" component={Notes} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={SignUp} />
